52954f4613bd7625f1fa04383905749507f3572a
RodrigoGordo <101176934+RodrigoGordo@users.noreply.github.com>
2025-01-31 15:51:59 +0000

    Contact us e css. Logo fixed. Sign in agora é um botão na nav bar

--- plantsrpetsprojeto.client/src/app/app-routing.module.ts
+++ plantsrpetsprojeto.client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FeaturesComponent } from './features/features.component';
 import { FaqComponent } from './faq/faq.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'features', component: FeaturesComponent },
-  { path: 'faq', component: FaqComponent }
+  { path: 'faq', component: FaqComponent },
+  { path: 'contact-us', component: ContactUsComponent}
 ];
 
 @NgModule({
--- plantsrpetsprojeto.client/src/app/app.module.ts
+++ plantsrpetsprojeto.client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FeaturesComponent } from './features/features.component';
 import { FaqComponent } from './faq/faq.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FaqComponent } from './faq/faq.component';
     HomeComponent,
     AboutUsComponent,
     FeaturesComponent,
-    FaqComponent
+    FaqComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
