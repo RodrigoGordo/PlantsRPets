3196f793991aededf8ac22c486b59b42e05d8ec6
RodrigoGordo <101176934+RodrigoGordo@users.noreply.github.com>
2025-02-05 18:23:07 +0000

    Forgot password fixed

--- PlantsRPetsProjeto.Server/obj/Debug/net8.0/PlantsRPetsProjeto.Server.AssemblyInfo.cs
+++ PlantsRPetsProjeto.Server/obj/Debug/net8.0/PlantsRPetsProjeto.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d5a7e1726bedaa1b7a5eea64f6ff4ebc8e92c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d067dcc1d7e55d7490083f6388ea5eedd1da6a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
