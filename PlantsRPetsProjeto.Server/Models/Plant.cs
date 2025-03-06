e05fd6a2e219703a92077986e6e78a6b435a0567
Ruben Pereira <202001215@estudantes.ips.pt>
2025-03-06 19:03:15 +0000

    Plant model

--- PlantsRPetsProjeto.Server/Models/Plant.cs
+++ PlantsRPetsProjeto.Server/Models/Plant.cs
@@ -1,13 +1,30 @@
-﻿namespace PlantsRPetsProjeto.Server.Models
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlantsRPetsProjeto.Server.Models
 {
     public class Plant
     {
         public int PlantId { get; set; }
         public string PlantName { get; set; }
-        public PlantType Type { get; set; }
-        public int GrowthTime { get; set; }
-        public int WaterFrequency { get; set; }
-        public bool isGrown { get; set; }
+        public string PlantType { get; set; }
+        public string Cycle { get; set; }
+        public string Watering { get; set; }
+        public List<string> PruningMonth { get; set; }
+        public string PruningCount { get; set; }
+        public string GrowthRate { get; set; }
+        public string Sun { get; set; }
+        public string Edible { get; set; }
+        public string CareLevel { get; set; }
+        public string Flowers { get; set; }
+        public string Fruits { get; set; }
+        public bool Leaf { get; set; }
+        public string Maintenance { get; set; }
+        public string SaltTolerant { get; set; }
+        public bool Indoor { get; set; }
+        public string SunDuration { get; set; }
+        public string FloweringSeason { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
     }
 
 }
