eb2d842ec3fa9bf0eb8db4ccebff63e375b46c40
Guilherme-Camarada <202002019@estudantes.ips.pt>
2025-05-05 09:28:39 +0100

    plant info page image fix
    fixed plants that have no image not replacing to the default image

--- PlantsRPetsProjeto.Server/obj/Debug/net8.0/PlantsRPetsProjeto.Server.AssemblyInfo.cs
+++ PlantsRPetsProjeto.Server/obj/Debug/net8.0/PlantsRPetsProjeto.Server.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcada88430c21f8a71f94804e4b9f969cdb756b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515ead6fa4a0b290bb003c1c1ce318aefd0bc43b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantsRPetsProjeto.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
